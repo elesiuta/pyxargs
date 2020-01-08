139331466e78634f0770a50bb5cb8ff51f712cba
eric <elesiuta@gmail.com>
2020-01-08 11:57:42 -0500

    version 1.0.0

--- pyxargs.py
+++ pyxargs.py
@@ -11,6 +11,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
 import os
 import re
 import csv
@@ -333,3 +334,4 @@ def main():
 
 if __name__ == "__main__":
     sys.exit(main())
+
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyxargs",
-    version="0.11.1",
+    version="1.0.0",
     description="A mostly complete implementation of xargs in python with some added features",
     long_description=long_description,
     long_description_content_type="text/markdown",
