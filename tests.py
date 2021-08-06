01a1a3ff2fb29d9236d342192687009c36075cb6
eric <elesiuta@gmail.com>
2021-08-06 16:30:27 -0400

    update tests

--- tests.py
+++ tests.py
@@ -16,13 +16,13 @@ class TestPyxargs(unittest.TestCase):
         shutil.rmtree("__pycache__", True)
 
     def test_stdin(self):
-        cmd = "echo hello world | python pyxargs.py -m stdin \"echo out {}\""
+        cmd = "echo hello world | python pyxargs.py -m stdin echo out {}"
         with os.popen(cmd) as result:
             result = result.readlines()
             self.assertEqual(result, ['out hello\n', 'out world\n'])
 
     def test_invoke_pyxargs_from_install(self):
-        cmd = "echo hello world | pyxargs -m stdin \"echo out {}\""
+        cmd = "echo hello world | pyxargs -m stdin echo out {}"
         with os.popen(cmd) as result:
             result = result.readlines()
             self.assertEqual(result, ['out hello\n', 'out world\n'])
@@ -40,25 +40,25 @@ class TestPyxargs(unittest.TestCase):
             self.assertEqual(result, ['out1 hello\n', 'out2 hello\n', 'out1 world\n', 'out2 world\n'])
 
     def test_read_files(self):
-        cmd = "python pyxargs.py -r \"(\.git|__pycache__)\" -o \"echo out {}\""
+        cmd = "python pyxargs.py -r \"(\.git|__pycache__)\" -o echo out {}"
         with os.popen(cmd) as result:
             result = result.readlines()
             self.assertEqual(result, ['out LICENSE\n', 'out README.md\n', 'out pyxargs.py\n', 'out setup.py\n', 'out test.txt\n', 'out tests.py\n'])
 
     def test_re_filter_file_path(self):
-        cmd = "python pyxargs.py -r \"\Aconfig\" \"echo out {}\""
+        cmd = "python pyxargs.py -r \"\Aconfig\" echo out {}"
         with os.popen(cmd) as result:
             result = result.readlines()
             self.assertEqual(result, [])
 
     def test_re_filter_file_name(self):
-        cmd = "python pyxargs.py -r \"\Aconfig\" -f \"echo out {}\""
+        cmd = "python pyxargs.py -r \"\Aconfig\" -f echo out {}"
         with os.popen(cmd) as result:
             result = result.readlines()
             self.assertEqual(result, ['out config\n'])
 
     def test_mode_path(self):
-        cmd = "python pyxargs.py -m path -r \"\Aconfig\" -f \"echo out {}\""
+        cmd = "python pyxargs.py -m path -r \"\Aconfig\" -f echo out {}"
         with os.popen(cmd) as result:
             result = result.readlines()
             if os.name == "nt":
@@ -67,25 +67,25 @@ class TestPyxargs(unittest.TestCase):
                 self.assertEqual(result, ['out .git/config\n'])
 
     def test_filter_file_extension(self):
-        cmd = "python pyxargs.py -r \".+\.py$\" \"echo out {}\""
+        cmd = "python pyxargs.py -r \".+\.py$\" echo out {}"
         with os.popen(cmd) as result:
             result = result.readlines()
             self.assertEqual(result, ['out pyxargs.py\n', 'out setup.py\n', 'out tests.py\n'])
 
     def test_resub(self):
-        cmd = "python pyxargs.py -r \".+\.py$\" --resub \"\.py\" \".txt\" \"{}\" \"echo out {}\""
+        cmd = "python pyxargs.py -r \".+\.py$\" --resub \"\.py\" \".txt\" \"{}\" echo out {}"
         with os.popen(cmd) as result:
             result = result.readlines()
             self.assertEqual(result, ['out pyxargs.txt\n', 'out setup.txt\n', 'out tests.txt\n'])
 
     def test_re_omit(self):
-        cmd = "python pyxargs.py -r \"(.+\.py)|(\.git)\" -o \"echo out {}\""
+        cmd = "python pyxargs.py -r \"(.+\.py)|(\.git)\" -o echo out {}"
         with os.popen(cmd) as result:
             result = result.readlines()
             self.assertEqual(result, ['out LICENSE\n', 'out README.md\n', 'out test.txt\n'])
 
     def test_stdin_delimiter(self):
-        cmd = "echo hello,world,bye,world | python pyxargs.py -d , \"echo out {}\""
+        cmd = "echo hello,world,bye,world | python pyxargs.py -d , echo out {}"
         with os.popen(cmd) as result:
             result = result.readlines()
             self.assertEqual(result, ['out hello\n', 'out world\n', 'out bye\n', 'out world\n'])
@@ -138,17 +138,17 @@ class TestPyxargs(unittest.TestCase):
             result = result.readlines()
             self.assertEqual(result, ['hello\n', 'world\n', 'bye\n', 'world\n', '4.0\n'])
 
-    def test_norun(self):
-        cmd = "echo hello world | python pyxargs.py -m stdin --norun \"echo out {}\""
+    def test_dryrun(self):
+        cmd = "echo hello world | python pyxargs.py -m stdin --dry-run echo out {}"
         with os.popen(cmd) as result:
             result = result.readlines()
             self.assertEqual(result, ['echo out hello\n', 'echo out world\n'])
 
     def test_read_items_cat_type(self):
         if os.name == "nt":
-            cmd = "type test.txt | pyxargs -m stdin echo out {}"
+            cmd = "type test.txt | python pyxargs.py -m stdin echo out {}"
         elif os.name == "posix":
-            cmd = "cat test.txt | pyxargs -m stdin echo out {}"
+            cmd = "cat test.txt | python pyxargs.py -m stdin echo out {}"
         else:
             self.assertEqual(True, False, "Unrecognized OS by this test")
         with open("test.txt", "r") as f:
